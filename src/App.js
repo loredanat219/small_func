b6f52c8b9e95ada0502897a7d3fefd8fdd83e4ae
I550563 <loredana.toma@sap.com>
2023-07-04 13:19:50 +0300

    small functionalities

--- src/App.js
+++ src/App.js
@@ -1,32 +1,172 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+// import "./App.css";
 
+// function MyButton() {
+//   return <button>I am a button</button>;
+// }
 
+// const user = {
+//   name: "Hedy Lamarr",
+//   imageUrl: "https://i.imgur.com/yXOvdOSs.jpg",
+//   imageSize: 90,
+// };
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//         <MyButton />
+//         <>
+//         <h1>{user.name}</h1>
+//         <img
+//         className = "avatar"
+//         src={user.imageUrl}
+//         alt={'Photo of ' + user.name}
+//         style={{
+//           width: user.imageSize,
+//           height: user.imageSize
+//         }}
+
+//       />
+//         </>
+
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+//END
+//...........................................................................................................
+import { useState } from 'react';
 function MyButton() {
-  return (
-    <button>I am a button</button>
-  );
+  return <button>I'm a button</button>;
 }
-function App() {
+function AboutPage() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <MyButton/>
-      </header>
-    </div>
+    <>
+      <h1>About</h1>
+      <p>
+        Hello there.
+        <br />
+        How do you do?
+      </p>
+    </>
   );
 }
 
-export default App;
+const user = {
+  name: "Hedy Lamarr",
+  imageUrl: "https://i.imgur.com/yXOvdOSs.jpg",
+  imageSize: 90,
+};
+
+const products = [
+  { title: 'Cabbage', isFruit: false, id: 1 },
+  { title: 'Garlic', isFruit: false, id: 2 },
+  { title: 'Apple', isFruit: true, id: 3 },
+];
+
+function GetListItems() {
+  const listItems = products.map((product) => (
+    <li
+      key={product.id}
+      style={{
+        color: product.isFruit ? "magenta" : "darkgreen",
+      }}
+    >
+      {product.title}
+    </li>
+  ));
+
+  return <ul>{listItems}</ul>;
+}
+
+function GetImage() {
+  return (
+    <>
+      <h1>{user.name}</h1>
+      <img
+        className="avatar"
+        src={user.imageUrl}
+        alt={"Photo of " + user.name}
+        style={{
+          width: user.imageSize,
+          height: user.imageSize,
+        }}
+      />
+    </>
+  );
+}
+
+function NewButton() {
+  function handleClick() {
+    alert('You clicked me!');
+  }
+
+  return (
+    <button onClick={handleClick}>
+      Click me
+    </button>
+  );
+}
+
+function SecondButton() {
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    setCount(count + 1);
+  }
+
+  return (
+    <button onClick={handleClick}>
+      Clicked {count} times
+    </button>
+  );
+}
+
+function ThirdButton({ count, onClick }) {
+  return (
+    <button onClick={onClick}>
+      Clicked {count} times
+    </button>
+  );
+}
+
+export default function MyApp() {
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    setCount(count + 1);
+  }
+  return (
+    <div>
+      <h1>Welcome to my app</h1>
+      <MyButton />
+      <AboutPage />
+      <GetImage />
+      <GetListItems />
+      <NewButton />
+      <SecondButton />
+      <div>
+      <h1>Counters that update together</h1>
+      <ThirdButton count={count} onClick={handleClick} />
+      <ThirdButton count={count} onClick={handleClick} />
+    </div>
+  </div>
+    
+  );
+}
+
